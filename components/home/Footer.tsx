6deba0d92c90da5cfae2a8db9420c9808e2d97be
vscode LeapTech devcontainer <valentin.rudloff.perso@gmail.com>
2024-03-13 14:21:35 +0000

    add font

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -1,12 +1,15 @@
 import RetroGrid from "../magicui/retro-grid";
 import { HomeSection } from "./components/HomeSection";
+import localFont from 'next/font/local'
+
+const myFont = localFont({ src: '../../src/aiop-font.ttf' })
 
 export const Footer = () => (
   <HomeSection className="max-w-none px-0 sm:px-0 xl:px-0 last:pb-0">
     <div className="relative flex h-full w-full items-center justify-center overflow-hidden border-b py-20 lg:py-32">
-      <div className="pointer-events-none whitespace-pre-wrap z-10 flex justify-around px-5 max-w-7xl w-full text-3xl font-mono lowercase sm:text-4xl lg:text-5xl xl:text-6xl">
+      <div className="pointer-events-none whitespace-pre-wrap z-10 flex justify-around px-5 max-w-7xl w-full text-3xl lowercase sm:text-4xl lg:text-5xl xl:text-6xl">
         {["A", "I", "O", "P"].map((letter) => (
-          <span key={letter}>{letter}</span>
+          <span key={letter} className={myFont.className}>{letter}</span>
         ))}
       </div>
       <RetroGrid />
