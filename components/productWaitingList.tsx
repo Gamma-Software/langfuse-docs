d563eaeda9e0ef5537eb61a36eefc04a79be38ec
vscode LeapTech devcontainer <valentin.rudloff.perso@gmail.com>
2024-03-15 23:58:18 +0000

    Fix import paths for Button component

--- components/productWaitingList.tsx
+++ components/productWaitingList.tsx
@@ -4,7 +4,7 @@ import { Header } from "@/components/Header";
 
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "@/components/ui/shadcnbutton";
+import { Button } from "@/components/ui/shadcn/button";
 import Link from "next/link";
 import {
   Form,
