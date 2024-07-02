7af7b3ba3776b1f526158be264938db5432100f8
vscode LeapTech devcontainer <valentin.rudloff.perso@gmail.com>
2024-07-02 11:41:45 +0000

    better embed yt video

---
+++ components/ui/YoutubeEmbed.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+export const YouTubeEmbed = ({
+  embedId
+}: {
+  embedId: string
+}) => {
+  return(<div className="aspect-w-16 aspect-h-9">
+    <iframe
+      className="w-full h-full rounded-lg"
+      src={`https://www.youtube.com/embed/${embedId}`}
+      title="YouTube video player"
+      width="100%"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>)
+};
+
+export default YouTubeEmbed;
